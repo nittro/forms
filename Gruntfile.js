3eed414931ebed9e6fc5ad4f1d011f14fc7e9883
danik <me@subsonic.cz>
2016-07-23 19:48:43 +0200

    compatibility-related changes:  - added nittro.json with filelist and compat flags

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,6 @@
 module.exports = function (grunt) {
 
-    var files = [
-        'src/js/Nittro/Forms/Vendor.js',
-        'src/js/Nittro/Forms/FormData.js',
-        'src/js/Nittro/Forms/Form.js',
-        'src/js/Nittro/Forms/Locator.js',
-        'src/js/Nittro/Forms/Bridges/FormsDI.js'
-    ];
+    var files = grunt.file.readJSON('nittro.json').files;
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -18,7 +12,7 @@ module.exports = function (grunt) {
             },
             nittro: {
                 files: {
-                    'dist/js/nittro-forms.min.js': files
+                    'dist/js/nittro-forms.min.js': files.js
                 }
             }
         },
@@ -29,13 +23,13 @@ module.exports = function (grunt) {
             },
             nettejs: {
                 files: {
-                    'dist/js/nittro-forms.js': files
+                    'dist/js/nittro-forms.js': files.js
                 }
             }
         },
 
         jasmine: {
-            src: files,
+            src: files.js,
             options: {
                 vendor: [
                     'bower_components/promiz/promiz.min.js',
