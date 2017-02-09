6928e445cf431aade6a60143d26db0ac2a0247f8
danik <me@subsonic.cz>
2017-02-10 00:12:26 +0100

    Refactoring ahead of 2.0

--- loader.js
+++
@@ -1,5 +0,0 @@
-/* jahudka/minify compatible loader */
-/*! @include file="src/js/Nittro/Forms/Vendor.js" */
-/*! @include file="src/js/Nittro/Forms/FormData.js" */
-/*! @include file="src/js/Nittro/Forms/Form.js" */
-/*! @include file="src/js/Nittro/Forms/Locator.js" */
