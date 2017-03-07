449470bac785c6ead49dc701df3b11e5143f2c77
danik <me@subsonic.cz>
2017-03-07 22:43:45 +0100

    removed client-side mime type validation

--- src/Vendor.js
+++ src/Vendor.js
@@ -1,35 +1,9 @@
 _context.invoke('Nittro.Forms', function () {
 
     if (!window.Nette || !window.Nette.validators) {
-        throw new Error('Nette/Forms vendor netteForms.js asset has not been loaded');
-
+        throw new Error('netteForms.js asset from Nette/Forms has not been loaded');
     }
 
-    var VendorForms = window.Nette;
-    _context.register(VendorForms, 'Vendor');
-
-    VendorForms.validators.mimeType = function(elem, arg, val) {
-        if (!val || !window.File || val.length && !(val[0] instanceof window.File)) {
-            return true;
-        }
-
-        if (!Array.isArray(arg)) {
-            arg = arg.trim().split(/\s*,\s*/g);
-        }
-
-        try {
-            if (!val.length) return false;
-
-            for (var i = 0; i < val.length; i++) {
-                if (val[i].type && arg.indexOf(val[i].type) === -1 && arg.indexOf(val[i].type.replace(/\/.*/, '/*')) === -1) {
-                    return false;
-
-                }
-            }
-        } catch (e) {}
-
-        return true;
-
-    };
+    _context.register(window.Nette, 'Vendor');
 
 });
